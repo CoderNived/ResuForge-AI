cc99a3577dff769eccd92dc08222699595879a0d
CoderNived <nivedshenoy0@gmail.com>
2026-04-07 22:55:49 +0530

    updating ai-resume-builder\src\routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-const page = () => {
-  return(
-    <div>
-      <h1>AI Resume Builder</h1>
-    </div>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default page;
+export default App;
